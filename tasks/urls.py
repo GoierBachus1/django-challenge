46501c6517e2d3f9d2170d421e8e3c1839229efe
GoierBachus1 <etza123les@gmail.com>
2025-10-21 12:17:34 -0600

    fix(routes): remove extra space in task_list URL name

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tasks/",views.task_list, name = " task_list"),
+    path("tasks/",views.task_list, name = "task_list"),
     path("tasks/new/", views.task_create, name = "task_create"),
     path("tasks/<int:pk>/",views.task_detail, name = "task_detail"),
     path("tasks/<int:pk>/edit/",views.task_update, name= "task_update"),
