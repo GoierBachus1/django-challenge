739d5a2e0c546e60888ed7065edc47577c958b67
GoierBachus1 <etza123les@gmail.com>
2025-10-21 11:16:20 -0600

    feat(tasks): add CRUD views, URLs and ModelForm for Task management

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.order_by("-created_at") #el - es ordenado por fecha de creación (de la reciente a la antigua)#
+    return render(request, "tasks_list.html",{"tasks":tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(task,pk=pk)
+    return render(request, "task_detail.html", {"task":task})
+
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect("task_detail", pk=task.pk)
+    else:
+        formm = TaskForm()
+    return render(request, "task_form.html",{"form":form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return redirect("task_detail", pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "task_form.html",{"form":form, "task":task})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task,pk=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect("task_list")
+    return render(request, "task_confirm_delete.html",{"task":task})
